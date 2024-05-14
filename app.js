e547479566e427c71ee4a44d64a22aa4a3ac9c07
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-14 16:49:59 -0400

    Built a function that takes an array and return the average of its elements

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 
 //Function that takes an array of numbers and return the sum
 let arr = [1,2,3,4];
-let sum = 0;
+
 function sumOfNumbers(arr){
-for(let i=0; i< arr.length; i++){
-    sum+=arr[i];
-}
-return sum;
+    let sum = 0;
+    for(let i=0; i< arr.length; i++){
+        sum+=arr[i];
+    }
+    return sum;
 }
 
 console.log(sumOfNumbers(arr))
+
+//Function that takes an array of numbers and return the averge.
+function getAverage(arr){
+    let sum = 0;
+    let len = arr.length
+    for(let i=0; i< len; i++){
+        sum+=arr[i];
+    }
+    let avg = sum/len;
+    console.log(sum, len)
+    return avg;
+}
+console.log(getAverage(arr))
+
